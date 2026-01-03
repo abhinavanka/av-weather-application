3d998ff4ae0a1532d971117b81bee5e3e05ab9ef
Abhina Vanka <vankaabhina21@gmail.com>
2026-01-03 15:22:35 +0530

    Fix Frontend handling for netlify function

--- script.js
+++ script.js
@@ -3,43 +3,45 @@ function getWeather() {
   let city = document.getElementById("city").value;
   let apiKey = "your_api_key";
 
-  let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-
+  let url = `/.netlify/functions/weather?city=${city}`;
   fetch(url)
-    .then(response => response.json())
-    .then(data => {
+  .then(response => {
+    if (!response.ok) {
+      throw new Error("API error");
+    }
+    return response.json();
+  })
+  .then(data => {
 
-      let temp = data.main.temp;
-      let desc = data.weather[0].description;
-      let condition = data.weather[0].main;
+    let temp = data.main.temp;
+    let desc = data.weather[0].description;
+    let condition = data.weather[0].main;
 
-      let weatherIcon = "";
+    let weatherIcon = "";
 
-      if (condition === "Clear") {
-        weatherIcon = "☀️";
-      } else if (condition === "Clouds") {
-        weatherIcon = "☁️";
-      } else if (condition === "Rain") {
-        weatherIcon = "🌧️";
-      } else if (condition === "Drizzle") {
-        weatherIcon = "🌦️";
-      } else if (condition === "Thunderstorm") {
-        weatherIcon = "⛈️";
-      } else if (condition === "Snow") {
-        weatherIcon = "❄️";
-      } else {
-        weatherIcon = "🌫️";
-      }
+    if (condition === "Clear") {
+      weatherIcon = "☀️";
+    } else if (condition === "Clouds") {
+      weatherIcon = "☁️";
+    } else if (condition === "Rain") {
+      weatherIcon = "🌧️";
+    } else if (condition === "Drizzle") {
+      weatherIcon = "🌦️";
+    } else if (condition === "Thunderstorm") {
+      weatherIcon = "⛈️";
+    } else if (condition === "Snow") {
+      weatherIcon = "❄️";
+    } else {
+      weatherIcon = "🌫️";
+    }
 
-      document.getElementById("result").innerHTML =
-        `
-        <div class="icon">${weatherIcon}</div>
-        <div class="temp">${temp} °C</div>
-        <div class="desc">${desc}</div>
-        `;
-    })
-    .catch(() => {
-      document.getElementById("result").innerHTML =
-        "City not found or API error";
-    });
-}
+    document.getElementById("result").innerHTML = `
+      <div class="icon">${weatherIcon}</div>
+      <div class="temp">${temp} °C</div>
+      <div class="desc">${desc}</div>
+    `;
+  })
+  .catch(() => {
+    document.getElementById("result").innerHTML =
+      "City not found or API error";
+  });
